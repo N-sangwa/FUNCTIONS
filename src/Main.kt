ed70b7268d91969830e6333a75c1538ebf036164
N-sangwa <nadiinesangwa@gmail.com>
2022-02-20 22:37:39 +0300

    I commit

---
+++ src/Main.kt
@@ -0,0 +1,25 @@
+fun main(){
+    var greetings = ("Hello Nadine")
+    println(greetings)
+    var remainder = remainder(5,2)
+    println(remainder)
+    var sum= sum(10,20,30,40)
+    println(sum)
+    text()
+}
+fun greetings(Name: String){
+    println("Hello" + Name)
+}
+fun remainder(e:Int, f:Int): Int {
+    var remainder = e % f
+    return remainder
+}
+    fun sum(w:Int, x:Int, y:Int, z:Int): Int{
+    var sum = (w + x + y + z)
+        return sum
+    }
+fun text (){
+    var text = ("I am a positive person")
+    println(text)
+}
+
